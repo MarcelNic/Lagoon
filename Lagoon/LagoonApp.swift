818b1979111448ac188898d8548c9a9db082ea42
MarcelNic <marcelnicaeus@gmail.com>
2026-01-11 22:27:36 +0100

    Initial Commit

---
+++ Lagoon/LagoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LagoonApp.swift
+//  Lagoon
+//
+//  Created by Marcel Nicaeus on 11.01.26.
+//
+
+import SwiftUI
+
+@main
+struct LagoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
